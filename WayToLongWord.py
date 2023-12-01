0b1aa8534be8eb2312fe1a6b277be437322d9ffc
Md. Fazle Rabbi <45620474+md-fazle@users.noreply.github.com>
2023-12-01 15:44:57 +0600

    Add files via upload

---
+++ WayToLongWord.py
@@ -0,0 +1,10 @@
+n=int(input())
+for i in range (0,n):
+    s=str(input())
+    l=len(s)
+    if l>10:
+        print(s[0], end="")
+        print(l-2, end="")
+        print(s[l-1])
+    else:
+        print(s)
